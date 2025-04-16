38357603490fd9dde8fe631ca3fd14eedf1cacfe
mike tihonchik <angry.guy.soft@gmail.com>
2025-04-15 21:42:10 -0600

    two-sum v2; try range rather than conventional for loop (slower)

--- two-sum/main.go
+++ two-sum/main.go
@@ -3,7 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+	fmt.Println(twoSumV2([]int{2, 7, 11, 15}, 9))
+}
+
+func twoSumV2(nums []int, target int) []int {
+	for i := range nums {
+		for j := range nums {
+			if i != j && nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
 }
 
 func twoSum(nums []int, target int) []int {
