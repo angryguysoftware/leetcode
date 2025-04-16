13807bd8f03c18fb07e7d3c12b3b9efee12e218a
mike tihonchik <angry.guy.soft@gmail.com>
2025-04-15 21:34:07 -0600

    simple solution for two-sum problem

---
+++ two-sum/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+}
+
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := 0; j < len(nums); j++ {
+			if i != j && nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
